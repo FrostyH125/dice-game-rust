c30e9323c849e97256f4d6917918da25d762f833
Zander_CachyOS <zandersylvester@gmail.com>
2026-02-25 08:13:44 -1000

    snake eyes update finished, need to impl snake update & draw to control it

--- src/entities/enemy_dice_boxes/snake_eyes.rs
+++ src/entities/enemy_dice_boxes/snake_eyes.rs
@@ -1,15 +1,21 @@
 use basic_raylib_core::graphics::sprite::Sprite;
-use raylib::{color::Color, math::{Rectangle, Vector2}, prelude::{RaylibDraw, RaylibDrawHandle}, text::Font, texture::Texture2D};
-
-use crate::entities::{
-    dice::{DICE_WIDTH_HEIGHT, Dice},
-    dice_box_data::{DiceBoxData, DiceBoxState},
+use raylib::{
+    color::Color,
+    math::{Rectangle, Vector2},
+    prelude::{RaylibDraw, RaylibDrawHandle},
+    text::Font,
+    texture::Texture2D,
 };
 
-static PLACEHOLDER_DICE_SPRITE: Sprite = Sprite::new( 64.0, 128.0, DICE_WIDTH_HEIGHT, DICE_WIDTH_HEIGHT);
+use crate::entities::{
+    dice::{DICE_WIDTH_HEIGHT, Dice, DiceState},
+    dice_box_data::{DICE_POINT_OFFSET_FOR_DETECTING_IF_INSIDE_BOX, DiceBoxData, DiceBoxState},
+};
+
+static PLACEHOLDER_DICE_SPRITE: Sprite = Sprite::new(64.0, 128.0, DICE_WIDTH_HEIGHT, DICE_WIDTH_HEIGHT);
 static SNAKE_EYES_DICE_BOX_SPRITE: Sprite = Sprite::new(14.0, 128.0, 36.0, 16.0);
-const SNAKE_EYES_DICE_DRAW_START_OFFSET: Vector2 = Vector2 { x: 2.0 + DICE_WIDTH_HEIGHT,  y: -15.0};
-const SNAKE_EYES_PLACEHOLDER_DICE_DRAW_START_OFFSET: Vector2 = Vector2 { x: 2.0, y: -15.0};
+const SNAKE_EYES_DICE_DRAW_START_OFFSET: Vector2 = Vector2 { x: 2.0 + DICE_WIDTH_HEIGHT, y: -15.0 };
+const SNAKE_EYES_PLACEHOLDER_DICE_DRAW_START_OFFSET: Vector2 = Vector2 { x: 2.0, y: -15.0 };
 
 pub struct SnakeEyes {
     data: DiceBoxData,
@@ -31,11 +37,17 @@ impl SnakeEyes {
     }
 
     pub fn update(&mut self, dice_in_hand: &mut Vec<Dice>, dt: f32) {
-        // waiting for dice: if dice is not 1, return to hand
-        // if dice.len > 2, return dice to hand
-        // tallying, if contains 2 1s, tally = 25, get value, waiting for action
-        // if empty, inactive
-        // controller will take over on waiting for action and do what they want with the value
+        match self.data.state {
+            DiceBoxState::WaitingForDice => {
+                self.snake_eyes_set_dice_positions();
+            }
+            DiceBoxState::TallyingPoints => {
+                self.data.total_value_for_current_round = self.tally_snake_eyes();
+                self.data.state = DiceBoxState::WaitingForAction;
+            },
+            DiceBoxState::WaitingForAction => (),
+            DiceBoxState::Inactive => (),
+        }
     }
 
     pub fn draw(&mut self, d: &mut RaylibDrawHandle, texture: &Texture2D, font: &Font) {
@@ -50,29 +62,87 @@ impl SnakeEyes {
                     self.data.dice_collect_rect.height as i32,
                     Color::WHITE,
                 );
-                self.draw_dice(d, texture);
+                self.snake_eyes_draw_dice(d, texture);
                 self.draw_placeholder_dice(d, texture);
             }
         }
-        
+
         // draw tally()
         // draw_snake_eyes()
-        
     }
     
-    fn draw_dice(&self, d: &mut RaylibDrawHandle, texture: &Texture2D) {
-        let mut draw_pos = self.data.pos + SNAKE_EYES_DICE_DRAW_START_OFFSET;
+    fn tally_snake_eyes(&self) -> i64 {
         
-        //can only be 0, 1 or 2
-        for i in 0..self.data.dice_in_box.len() {
-            self.data.dice_in_box[i].draw(d, texture);
-            draw_pos.x -= DICE_WIDTH_HEIGHT;
+        let mut num_of_ones = 0;
+        
+        for dice in &self.data.dice_in_box {
+            if dice.value == 1 {
+                num_of_ones += 1;
+            }
+        }
+        
+        if num_of_ones >= 2 {
+            return 25;
+        } else {
+            return 0;
         }
     }
     
+    fn check_for_two_dice_with_value_one_in_hand(dice_in_hand: &[Dice]) -> bool {
+        
+        let mut num_of_ones = 0;
+        
+        for dice in dice_in_hand {
+            if dice.value == 1 {
+                num_of_ones += 1;
+            }
+        }
+        
+        if num_of_ones >= 2 {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    //this will only be for player usage
+    fn snake_eyes_check_dice_dragging_into_box(&mut self, dice_in_hand: &mut Vec<Dice>) {
+        for i in (0..dice_in_hand.len()).rev() {
+            if dice_in_hand[i].state == DiceState::Stopped
+                && self
+                    .data
+                    .dice_collect_rect
+                    .check_collision_point_rec(dice_in_hand[i].pos + DICE_POINT_OFFSET_FOR_DETECTING_IF_INSIDE_BOX)
+            {
+                if dice_in_hand[i].value != 1 {
+                    return;
+                }
+                let dice_to_add = dice_in_hand.remove(i);
+                self.data.dice_in_box.push(dice_to_add);
+                self.data.dice_in_box.sort_by(|a, b| a.value.cmp(&b.value));
+            }
+        }
+    }
+
+    fn snake_eyes_set_dice_positions(&mut self) {
+        let mut pos = self.data.pos + SNAKE_EYES_DICE_DRAW_START_OFFSET;
+
+        //can only be 0, 1 or 2
+        for i in 0..self.data.dice_in_box.len() {
+            self.data.dice_in_box[i].pos = Vector2 { x: pos.x, y: pos.y };
+            pos.x -= DICE_WIDTH_HEIGHT;
+        }
+    }
+
+    fn snake_eyes_draw_dice(&self, d: &mut RaylibDrawHandle, texture: &Texture2D) {
+        for i in 0..self.data.dice_in_box.len() {
+            self.data.dice_in_box[i].draw(d, texture);
+        }
+    }
+
     fn draw_placeholder_dice(&self, d: &mut RaylibDrawHandle, texture: &Texture2D) {
         let mut start_pos = self.data.pos + SNAKE_EYES_DICE_DRAW_START_OFFSET;
-        
+
         // the reverse of drawing the dice
         // if len is 1, this will only draw once, starting from the opposite side
         for _ in 0..2 - self.data.dice_in_box.len() {
@@ -80,12 +150,8 @@ impl SnakeEyes {
             start_pos.x -= DICE_WIDTH_HEIGHT;
         }
     }
-    
-    fn draw_snake_eyes_text() {
-        
-    }
-    
-    fn draw_25_if_score() {
-        
-    }
+
+    fn draw_snake_eyes_text() {}
+
+    fn draw_25_if_score() {}
 }
